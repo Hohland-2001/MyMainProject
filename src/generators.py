68a39d2f63544e3213aa1b5523e4a478d0b8c722
Max Yanchuk <maksim.yanchuk.01@mail.ru>
2025-07-07 20:13:22 +0300

    Добавлены тесты для модуля generators.py. Дополнен файл README.md

--- src/generators.py
+++ src/generators.py
@@ -1,78 +1,40 @@
-transactions = [
-    {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702",
-    },
-    {
-        "id": 142264268,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188",
-    },
-    {
-        "id": 873106923,
-        "state": "EXECUTED",
-        "date": "2019-03-23T01:09:46.296404",
-        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 44812258784861134719",
-        "to": "Счет 74489636417521191160",
-    },
-    {
-        "id": 895315941,
-        "state": "EXECUTED",
-        "date": "2018-08-19T04:27:37.904916",
-        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод с карты на карту",
-        "from": "Visa Classic 6831982476737658",
-        "to": "Visa Platinum 8990922113665229",
-    },
-    {
-        "id": 594226727,
-        "state": "CANCELED",
-        "date": "2018-09-12T21:27:25.241689",
-        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Перевод организации",
-        "from": "Visa Platinum 1246377376343588",
-        "to": "Счет 14211924144426031657",
-    },
-]
-
-
-def filter_by_currency(transactions: list, currency_code: str) -> str:
+def filter_by_currency(transactions: list, currency_code: str = '') -> str:
     """Функция принимает на вход список словарей, представляющих транзакции.
     Возвращает итератор, который поочередно выдает транзакции,
     где валюта операции соответствует заданной."""
-    for transaction in transactions:
-        if transaction["operationAmount"]["currency"]["code"] == currency_code:
-            yield transaction
+    if currency_code == '':
+        yield transactions
+    else:
+        for transaction in transactions:
+            if transaction["operationAmount"]["currency"]["code"] == currency_code:
+                yield transaction
 
 
 def transaction_descriptions(transactions: list) -> str:
-    for transaction in transactions:
-        yield transaction["description"]
+    """Принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
+    if transactions == []:
+        yield []
+    else:
+        for transaction in transactions:
+            yield transaction["description"]
 
 
 def card_number_generator(start: int = 1, stop: int = 9999999999999999) -> str:
-    for gen_num in range(start, stop + 1):
-        len_numbers = 16 - len(str(gen_num))
-        if gen_num <= stop:
-            card_number_gen = ("0" * len_numbers) + str(gen_num)
-            card_number_gen = (
-                card_number_gen[:4]
-                + " "
-                + card_number_gen[4:8]
-                + " "
-                + card_number_gen[8:12]
-                + " "
-                + card_number_gen[-4:]
-            )
-            yield card_number_gen
+    """Функция генерирует номера банковских карт в заданном диапозоне"""
+    if len(str(start)) <= 16 and len(str(stop)) <= 16:
+        for gen_num in range(start, stop + 1):
+            len_numbers = 16 - len(str(gen_num))
+            if gen_num <= stop:
+                card_number_gen = ("0" * len_numbers) + str(gen_num)
+                card_number_gen = (
+                    card_number_gen[:4]
+                    + " "
+                    + card_number_gen[4:8]
+                    + " "
+                    + card_number_gen[8:12]
+                    + " "
+                    + card_number_gen[-4:]
+                )
+                yield card_number_gen
+    else:
+        yield "Число не долно превышать 16 символов"

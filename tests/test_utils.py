48a12edae10646ca56db1a1db8e0f5c3230f9aee
Max Yanchuk <maksim.yanchuk.01@mail.ru>
2025-07-30 17:08:33 +0300

    Добавлены модули чтения файлов .csv и .xlsx

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,19 +1,6 @@
-import json
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 from src.utils import get_list_of_operations
-
-list_transaction = [
-    {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702",
-    }
-]
 
 
 @patch("builtins.open", create=True)
